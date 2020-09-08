296cfbb134be8d5eeb91c7f617fca0892a93d5e4
Corwin Kuiper <corwin@kuiper.dev>
2020-09-08 03:39:48 +0100

    moved parsing function to own file

---
+++ src/items.rs
@@ -0,0 +1,66 @@
+#[path = "stats.rs"]
+mod stats;
+
+pub struct Item {
+    pub name: String,
+    pub stats: stats::Characteristic,
+    pub level: i32,
+    pub set_id: Option<i32>,
+    pub dofus_id: i32,
+    pub restrictions: Option<stats::Restriction>,
+}
+
+pub fn parse_items(data: &[u8]) -> Vec<Item> {
+    let mut items = vec![];
+    let data: serde_json::Value = serde_json::from_slice(data).unwrap();
+    for item in data.as_array().unwrap() {
+        let name = item
+            .get("name")
+            .unwrap()
+            .get("en")
+            .unwrap()
+            .as_str()
+            .unwrap()
+            .to_string();
+        let level = item.get("level").unwrap().as_i64().unwrap() as i32;
+        let mut set_id = None;
+        if let Some(set) = item.get("setID") {
+            if let Some(set) = set.as_str() {
+                if let Ok(set) = set.parse() {
+                    set_id = Some(set)
+                }
+            }
+        }
+
+        let dofus_id: i32 = {
+            let id = item.get("dofusID").unwrap();
+            if let Some(id) = id.as_str() {
+                id.parse().unwrap()
+            } else if let Some(id) = id.as_i64() {
+                id as i32
+            } else {
+                panic!()
+            }
+        };
+        let mut stats: stats::Characteristic = [0; 51];
+        if let Some(stat) = item.get("stats") {
+            for stat in stat.as_array().unwrap() {
+                let characteristic = stat.get("stat").unwrap().as_str().unwrap();
+                let value = stat.get("maxStat").unwrap().as_i64().unwrap() as stats::StatValue;
+                let characteristic_index = stats::stat_from_str(characteristic).unwrap() as usize;
+                stats[characteristic_index] = value;
+            }
+        }
+
+        let restrictions = None; //todo
+        items.push(Item {
+            name,
+            level,
+            set_id,
+            dofus_id,
+            stats,
+            restrictions,
+        });
+    }
+    items
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod anneal;
+mod items;
 mod stats;
 
 use anyhow;
@@ -7,7 +8,13 @@ use anyhow;
 extern crate lazy_static;
 
 fn main() -> Result<(), anyhow::Error> {
-    for item in ITEMS.iter() {
+    print_items(&ITEMS);
+    print_items(&WEAPONS);
+    Ok(())
+}
+
+fn print_items(items: &Vec<items::Item>) {
+    for item in items.iter() {
         println!("============");
         println!("Name: {}", item.name);
         println!("Level: {}", item.level);
@@ -19,73 +26,10 @@ fn main() -> Result<(), anyhow::Error> {
             }
         }
     }
-    Ok(())
 }
 
 lazy_static! {
-    static ref ITEMS: Vec<Item> = {
-        let mut items = vec![];
-        let data: serde_json::Value =
-            serde_json::from_slice(include_bytes!("../data/items.json")).unwrap();
-        for item in data.as_array().unwrap() {
-            let name = item
-                .get("name")
-                .unwrap()
-                .get("en")
-                .unwrap()
-                .as_str()
-                .unwrap()
-                .to_string();
-            let level = item.get("level").unwrap().as_i64().unwrap() as i32;
-            let mut set_id = None;
-            if let Some(set) = item.get("setID") {
-                if let Some(set) = set.as_str() {
-                    if let Ok(set) = set.parse() {
-                        set_id = Some(set)
-                    }
-                }
-            }
-
-            let dofus_id: i32 = {
-                let id = item.get("dofusID").unwrap();
-                if let Some(id) = id.as_str() {
-                    id.parse().unwrap()
-                } else if let Some(id) = id.as_i64() {
-                    id as i32
-                } else {
-                    panic!()
-                }
-            };
-            let mut stats: stats::Characteristic = [0; 51];
-            if let Some(stat) = item.get("stats") {
-                for stat in stat.as_array().unwrap() {
-                    let characteristic = stat.get("stat").unwrap().as_str().unwrap();
-                    let value = stat.get("maxStat").unwrap().as_i64().unwrap() as stats::StatValue;
-                    let characteristic_index =
-                        stats::stat_from_str(characteristic).unwrap() as usize;
-                    stats[characteristic_index] = value;
-                }
-            }
-
-            let restrictions = None; //todo
-            items.push(Item {
-                name,
-                level,
-                set_id,
-                dofus_id,
-                stats,
-                restrictions,
-            });
-        }
-        items
-    };
-}
-
-struct Item {
-    name: String,
-    stats: stats::Characteristic,
-    level: i32,
-    set_id: Option<i32>,
-    dofus_id: i32,
-    restrictions: Option<stats::Restriction>,
+    static ref ITEMS: Vec<items::Item> = items::parse_items(include_bytes!("../data/items.json"));
+    static ref WEAPONS: Vec<items::Item> =
+        items::parse_items(include_bytes!("../data/weapons.json"));
 }
