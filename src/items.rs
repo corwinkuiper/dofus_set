6c889242ce53ad98390bacda1b4bcbcc02f2ce00
Gwilym Kuiper <gw@ilym.me>
2020-09-08 20:28:08 +0100

    Finish serdifying

--- src/items.rs
+++ src/items.rs
@@ -24,7 +24,7 @@ struct DofusLabItemName {
 struct DofusLabItemStats {
     stat: String,
     minStat: Option<i32>,
-    maxStat: Option<i32>,
+    maxStat: i32,
 }
 
 #[allow(non_snake_case)]
@@ -34,61 +34,28 @@ struct DofusLabItem {
     itemType: String,
     setID: Option<String>,
     stats: Vec<DofusLabItemStats>,
+    level: i32,
 }
 
 pub fn parse_items(data: &[u8]) -> Vec<Item> {
-    let mut items = vec![];
-    let data: serde_json::Value = serde_json::from_slice(data).unwrap();
-    for item in data.as_array().unwrap() {
-        let name = item
-            .get("name")
-            .unwrap()
-            .get("en")
-            .unwrap()
-            .as_str()
-            .unwrap()
-            .to_string();
-        let item_type = item.get("itemType").unwrap().as_str().unwrap().to_string();
-        let level = item.get("level").unwrap().as_i64().unwrap() as i32;
-        let mut set_id = None;
-        if let Some(set) = item.get("setID") {
-            if let Some(set) = set.as_str() {
-                if let Ok(set) = set.parse() {
-                    set_id = Some(set)
-                }
-            }
-        }
+    let data: Vec<DofusLabItem> = serde_json::from_slice(data).unwrap();
 
-        let dofus_id: i32 = {
-            let id = item.get("dofusID").unwrap();
-            if let Some(id) = id.as_str() {
-                id.parse().unwrap()
-            } else if let Some(id) = id.as_i64() {
-                id as i32
-            } else {
-                panic!()
+    data.iter()
+        .map(|item| {
+            let mut stats = stats::new_characteristics();
+            for stat in &item.stats {
+                let characteristic_index = stats::stat_from_str(&stat.stat).unwrap() as usize;
+                stats[characteristic_index] = stat.maxStat;
             }
-        };
-        let mut stats: stats::Characteristic = stats::new_characteristics();
-        if let Some(stat) = item.get("stats") {
-            for stat in stat.as_array().unwrap() {
-                let characteristic = stat.get("stat").unwrap().as_str().unwrap();
-                let value = stat.get("maxStat").unwrap().as_i64().unwrap() as stats::StatValue;
-                let characteristic_index = stats::stat_from_str(characteristic).unwrap() as usize;
-                stats[characteristic_index] = value;
-            }
-        }
 
-        let restrictions = None; //todo
-        items.push(Item {
-            item_type,
-            name,
-            level,
-            set_id,
-            dofus_id,
-            stats,
-            restrictions,
-        });
-    }
-    items
+            Item {
+                name: item.name.en.clone(),
+                item_type: item.itemType.clone(),
+                stats: stats,
+                dofus_id: 0,
+                level: item.level,
+                set_id: item.setID.as_ref().map(|id| id.parse().unwrap()),
+                restrictions: None,
+            }
+        }).collect()
 }
