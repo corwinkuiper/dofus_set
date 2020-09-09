67f3d800ca4314975ba99a844fb3be477d51699e
Gwilym Kuiper <gw@ilym.me>
2020-09-09 22:30:44 +0100

    Some form of select box

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import './App.css'
 
-function App() {
-  return (
-    <h1>Hello, World!</h1>
-  )
+import * as DofusStats from './dofus/stats'
+
+class Weight extends React.Component<{}, { value: number }> {
+  state = { value: 0 }
+
+  constructor(props: {}) {
+    super(props)
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  private handleChange(event: React.FormEvent<HTMLSelectElement>) {
+    this.setState({ value: parseInt(event.currentTarget.value, 10) })
+  }
+
+  render() {
+    return (
+      <select value={this.state.value} onChange={this.handleChange}>
+        {DofusStats.StatNames.map((statName, i) => <option value={i} key={i}>{statName}</option>)}
+      </select>
+    )
+  }
 }
+
+const App = () => <Weight />
 
 export default App
