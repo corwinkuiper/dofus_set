974bbf3ffc9cd2adfc6d146923f844daf30b7098
Gwilym Kuiper <gw@ilym.me>
2020-09-18 22:26:43 +0100

    Export Item class to a new file

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,36 +3,13 @@ import './App.css'
 import { StatNames } from './dofus/stats'
 
 import { OptimiseApi } from './dofus/OptimiseApi'
+import { Item } from './Item'
 
 import { WeightsSelector, WeightsState } from './WeightsSelector'
 import { Spinner } from './Spinner'
 
 function classNames(classes: { [className: string]: boolean }) {
   return Object.entries(classes).filter(entry => entry[1]).map(entry => entry[0]).join(' ')
-}
-
-function getImageUrl(imageUrl: string): string {
-  const suffix = imageUrl.slice(imageUrl.lastIndexOf('/') + 1)
-  return `https://d2iuiayak06k8j.cloudfront.net/item/${suffix}`
-}
-
-class Item {
-  readonly name: string
-  readonly characteristics: number[]
-  readonly level: number
-  readonly imageUrl?: string
-  readonly dofusId: number
-
-  constructor(name: string, characteristics: number[], level: number, imageUrl: string | undefined, dofusId: number) {
-    this.name = name
-    this.characteristics = characteristics
-    this.level = level
-    this.dofusId = dofusId
-
-    if (imageUrl) {
-      this.imageUrl = getImageUrl(imageUrl)
-    }
-  }
 }
 
 class SetBonus {
