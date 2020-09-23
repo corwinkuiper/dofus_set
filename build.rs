f00658ad987e55ef8a1aa4735d463bd46bc3aeba
Corwin Kuiper <corwin@kuiper.dev>
2020-09-23 13:36:43 +0100

    add watch files after building

--- build.rs
+++ build.rs
@@ -4,14 +4,6 @@ use walkdir::WalkDir;
 const WEB_DIRECTORY: &str = "web";
 
 fn main() -> Result<(), Box<dyn std::error::Error + Sync + Send>> {
-    for entry in WalkDir::new(WEB_DIRECTORY)
-        .into_iter()
-        .filter_map(Result::ok)
-        .filter(|e| !e.file_type().is_dir())
-    {
-        println!("cargo:rerun-if-changed={}", entry.path().to_string_lossy());
-    }
-
     Command::new("npm")
         .current_dir(WEB_DIRECTORY)
         .args(&["ci"])
@@ -20,6 +12,14 @@ fn main() -> Result<(), Box<dyn std::error::Error + Sync + Send>> {
         .current_dir(WEB_DIRECTORY)
         .args(&["run", "build"])
         .output()?;
+
+    for entry in WalkDir::new(WEB_DIRECTORY)
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|e| !e.file_type().is_dir())
+    {
+        println!("cargo:rerun-if-changed={}", entry.path().to_string_lossy());
+    }
 
     Ok(())
 }
