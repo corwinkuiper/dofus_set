49fca826fd4ae8bfa6458c97009448ace217bf87
Corwin Kuiper <corwin@kuiper.dev>
2020-09-23 13:52:09 +0100

    ignore node_modules, build, and files beginning with .

--- build.rs
+++ build.rs
@@ -16,6 +16,15 @@ fn main() -> Result<(), Box<dyn std::error::Error + Sync + Send>> {
     for entry in WalkDir::new(WEB_DIRECTORY)
         .into_iter()
         .filter_map(Result::ok)
+        .filter(|x| {
+            !x.path()
+                .starts_with(format!("{}/{}", WEB_DIRECTORY, "node_modules"))
+        })
+        .filter(|x| {
+            !x.path()
+                .starts_with(format!("{}/{}", WEB_DIRECTORY, "build"))
+        })
+        .filter(|x| !x.file_name().to_string_lossy().starts_with('.'))
     {
         println!("cargo:rerun-if-changed={}", entry.path().to_string_lossy());
     }
