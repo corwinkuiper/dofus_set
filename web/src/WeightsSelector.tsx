a4a9762cd9f31c952d691a9f96adf9ff6b93e7ea
Gwilym Kuiper <gw@ilym.me>
2020-09-28 23:55:59 +0100

    Add little icons in the exo boxes

--- web/src/WeightsSelector.tsx
+++ web/src/WeightsSelector.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as DofusStats from './dofus/stats'
+import { StatIcon } from './App/StatIcon';
 
 class StatSelector extends React.Component<{ onStatChange: (statId: number) => void, value: number }> {
   constructor(props: { onStatChange: (statId: number) => void, value: number }) {
@@ -80,15 +81,24 @@ export function ExoSelector({ exoOptions, updateExoOptions }: { exoOptions: ExoO
   return (
     <div className="exo-selector">
       <div className="exo-checkbox">
-        <label htmlFor="ap-exo">AP exo</label>
+        <label htmlFor="ap-exo">
+          <StatIcon statIndex={0} />
+          AP exo
+        </label>
         <input type="checkbox" id="ap-exo" checked={exoOptions.apExo} onChange={() => toggleExoValue('apExo')} />
       </div>
       <div className="exo-checkbox">
-        <label htmlFor="mp-exo">MP exo</label>
+        <label htmlFor="mp-exo">
+          <StatIcon statIndex={1} />
+          MP exo
+        </label>
         <input type="checkbox" id="mp-exo" checked={exoOptions.mpExo} onChange={() => toggleExoValue('mpExo')} />
       </div>
       <div className="exo-checkbox">
-        <label htmlFor="range-exo">Range exo</label>
+        <label htmlFor="range-exo">
+          <StatIcon statIndex={2} />
+          Range exo
+        </label>
         <input type="checkbox" id="range-exo" checked={exoOptions.rangeExo} onChange={() => toggleExoValue('rangeExo')} />
       </div>
     </div>
