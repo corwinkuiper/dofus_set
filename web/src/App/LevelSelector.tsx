a3d4a1921ab31a92b6a5ea2ffbc56f080c46dbe0
Gwilym Kuiper <gw@ilym.me>
2020-09-28 23:46:40 +0100

    Allow clearing of maximum level

--- web/src/App/LevelSelector.tsx
+++ web/src/App/LevelSelector.tsx
@@ -1,26 +1,42 @@
 import * as React from 'react'
 
-export class LevelSelector extends React.Component<{ maxLevel: number, setMaxLevel: (newMaxLevel: number) => void }> {
-    constructor(props: { maxLevel: number, setMaxLevel: (newMaxLevel: number) => void }) {
+interface LevelSelectorProps {
+    maxLevel: number
+    setMaxLevel: (newMaxLevel: number) => void
+}
+
+interface LevelSelectorState {
+    currentLevel: string
+}
+
+export class LevelSelector extends React.Component<LevelSelectorProps, LevelSelectorState> {
+    constructor(props: LevelSelectorProps) {
         super(props)
 
         this.maxLevelChanged = this.maxLevelChanged.bind(this)
+
+        this.state = { currentLevel: props.maxLevel.toString() }
     }
 
     private maxLevelChanged(event: React.FormEvent<HTMLInputElement>) {
-        const parsed = parseInt(event.currentTarget.value, 10)
+        const newLevel = event.currentTarget.value
+        const parsed = parseInt(newLevel, 10)
         if (isNaN(parsed)) {
+            if (newLevel === '') {
+                this.setState({ currentLevel: '' })
+            }
             return
         }
 
         this.props.setMaxLevel(Math.floor(parsed))
+        this.setState({ currentLevel: parsed.toString() })
     }
 
     render() {
         return (
             <div className="max-level">
-                <span>Maximum Level</span>
-                <input type="text" value={this.props.maxLevel.toString()} onChange={this.maxLevelChanged} />
+                <span>Maximum level</span>
+                <input type="text" value={this.state.currentLevel} onChange={this.maxLevelChanged} />
             </div>
         )
     }
