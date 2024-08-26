d887f3c9681b57e2f93cc9d1c00376877202a6d3
Gwilym Inzani <email@gwilym.dev>
2024-08-26 19:05:13 +0100

    use jemalloc in the benchmark (can move later)

--- bench/src/main.rs
+++ bench/src/main.rs
@@ -38,3 +38,10 @@ fn main() {
         println!("Set Energy: {}", -final_state.energy(&config, &items));
     }
 }
+
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
